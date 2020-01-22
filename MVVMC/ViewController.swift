42c66079a88b913ae49a3e60cd2a916361b7881c
Evan Xie <evanxie.mr@foxmail.com>
2020-01-22 10:08:20 +0800

    Add MVVM + Coordinator + RxSwift Project

---
+++ MVVMC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVVMC
+//
+//  Created by Evan Xie on 2020/1/22.
+//  Copyright © 2020 Evan Xie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,14 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'MVVMC' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for MVVMC
+  pod 'RxSwift'
+  pod 'RxCocoa'
+  pod 'NSObject+Rx'
+  pod 'RxDataSources'
+
+end
