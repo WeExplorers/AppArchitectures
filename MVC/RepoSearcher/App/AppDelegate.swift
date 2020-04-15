c05f2636dad308940de51ddcb92b8e5e49b963b6
Evan Xie <evanxie.mr@foxmail.com>
2020-04-15 18:29:54 +0800

    Add MVC sample

---
+++ MVC/RepoSearcher/App/AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.swift
+//  RepoSearcher
+//
+//  Created by Arthur Myronenko on 6/29/17.
+//  Copyright © 2017 UPTech Team. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+}
--- MVVMC/Networking/RestfulApi.swift
+++
@@ -1,7 +0,0 @@
-//
-//  RestfulApi.swift
-//  MVVMC
-//
-//  Created by Evan Xie on 2020/1/22.
-//  Copyright © 2020 Evan Xie. All rights reserved.
-//
